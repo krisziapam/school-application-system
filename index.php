acd8714f576cabf8fd6743d80d6f5ecb2b73e2f5
Krisziapam <193602661+krisziapam@users.noreply.github.com>
2026-04-28 06:51:56 +0800

    Enhance index.php with HTML layout and links
    Added HTML structure and navigation links to the application.

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-echo "School Application System";
+$page_title = "School Application System";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $page_title; ?></title>
+</head>
+<body>
+
+<h1>School Application System</h1>
+
+<p>Welcome to the school application system.</p>
+
+<ul>
+    <li><a href="applicants.php">View Applicants</a></li>
+    <li><a href="add_applicant.php">Add Applicant</a></li>
+</ul>
+
+</body>
+</html>
