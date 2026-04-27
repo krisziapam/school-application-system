df31b6a78328b206e81ca42e369844db793fdd47
Krisziapam <193602661+krisziapam@users.noreply.github.com>
2026-04-28 06:52:16 +0800

    Create add_applicant.php for applicant form

--- add_applicant.php
+++ add_applicant.php
@@ -1,1 +1,72 @@
+<?php
+include "config.php";
 
+$campuses = mysqli_query($conn, "SELECT * FROM campus");
+$categories = mysqli_query($conn, "SELECT * FROM educational_background_category");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Applicant</title>
+</head>
+<body>
+
+<h1>Add Applicant</h1>
+
+<form action="save_applicant.php" method="POST">
+
+    <label>First Name:</label><br>
+    <input type="text" name="first_name" required><br><br>
+
+    <label>Middle Name:</label><br>
+    <input type="text" name="middle_name"><br><br>
+
+    <label>Last Name:</label><br>
+    <input type="text" name="last_name" required><br><br>
+
+    <label>Sex:</label><br>
+    <select name="sex" required>
+        <option value="">Select Sex</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+    </select><br><br>
+
+    <label>Birth Date:</label><br>
+    <input type="date" name="birth_date"><br><br>
+
+    <label>Email:</label><br>
+    <input type="email" name="email"><br><br>
+
+    <label>Phone:</label><br>
+    <input type="text" name="phone"><br><br>
+
+    <label>Campus:</label><br>
+    <select name="campus_id" required>
+        <option value="">Select Campus</option>
+        <?php while ($campus = mysqli_fetch_assoc($campuses)) { ?>
+            <option value="<?php echo $campus['campus_id']; ?>">
+                <?php echo $campus['campus_name']; ?>
+            </option>
+        <?php } ?>
+    </select><br><br>
+
+    <label>Educational Background:</label><br>
+    <select name="educational_background_category_id" required>
+        <option value="">Select Educational Background</option>
+        <?php while ($category = mysqli_fetch_assoc($categories)) { ?>
+            <option value="<?php echo $category['educational_background_category_id']; ?>">
+                <?php echo $category['category_name']; ?>
+            </option>
+        <?php } ?>
+    </select><br><br>
+
+    <button type="submit">Save Applicant</button>
+
+</form>
+
+<br>
+<a href="applicants.php">Back to Applicants</a>
+
+</body>
+</html>
