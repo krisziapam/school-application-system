b8ea4f672341178f6049d4c05d912a69f4519e97
Krisziapam <193602661+krisziapam@users.noreply.github.com>
2026-04-28 06:52:36 +0800

    Add applicant data insertion functionality

--- save_applicant.php
+++ save_applicant.php
@@ -1,1 +1,25 @@
+<?php
+include "config.php";
 
+$first_name = $_POST['first_name'];
+$middle_name = $_POST['middle_name'];
+$last_name = $_POST['last_name'];
+$sex = $_POST['sex'];
+$birth_date = $_POST['birth_date'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$campus_id = $_POST['campus_id'];
+$educational_background_category_id = $_POST['educational_background_category_id'];
+
+$sql = "INSERT INTO applicant 
+(first_name, middle_name, last_name, sex, birth_date, email, phone, campus_id, educational_background_category_id)
+VALUES 
+('$first_name', '$middle_name', '$last_name', '$sex', '$birth_date', '$email', '$phone', '$campus_id', '$educational_background_category_id')";
+
+if (mysqli_query($conn, $sql)) {
+    header("Location: applicants.php");
+    exit();
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+?>
